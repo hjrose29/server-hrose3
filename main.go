845670501c6a6a702c033981e255c223980b4f5c
Henry <henryjrose2@gmail.com>
2023-10-26 18:22:41 -0400

    Basic gorilla mux server working and displaying timestamp

--- main.go
+++ main.go
@@ -1,5 +1,28 @@
 package main
+
+
 import (
     "fmt"
     "net/http"
+	"github.com/gorilla/mux"
+	"time"
 )
+func main() {
+
+	r := mux.NewRouter()
+	r.HandleFunc("/hrose3/status", statusHandler).Methods(http.MethodGet)
+	r.PathPrefix("/").HandlerFunc(catchAllHandler)
+    
+    http.ListenAndServe(":8080", r)
+
+}
+
+
+func statusHandler(w http.ResponseWriter, r *http.Request) {
+    w.WriteHeader(http.StatusOK)
+    fmt.Fprintf(w, time.Now().String())
+}
+
+func catchAllHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "404 - Page not found!")
+}
